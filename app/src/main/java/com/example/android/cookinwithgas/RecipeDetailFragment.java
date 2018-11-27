a6f2219ae47bc766e3f554b7757f860fa9bbb061
rowens918 <robert.owens918@gmail.com>
2018-11-27 11:22:38 -0600

    Udacity Grow with Google Android Nano Degree Project 4. (v2) A Baking App that loads a remote JSON, parses it, displays all the info, and plays videos via ExoPlayer, and utilizes a home page widget to display the ingredients list for the last recipe viewed.

--- app/src/main/java/com/example/android/cookinwithgas/RecipeDetailFragment.java
+++ app/src/main/java/com/example/android/cookinwithgas/RecipeDetailFragment.java
@@ -2,9 +2,11 @@ package com.example.android.cookinwithgas;
 
 import android.app.Activity;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,10 +23,10 @@ import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
-import com.google.android.exoplayer2.util.Util;
 import com.squareup.picasso.Picasso;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -42,16 +44,19 @@ public class RecipeDetailFragment extends Fragment {
      * represents.
      */
     public static final String ARG_ITEM_ID = "item_id";
+    public static final String POS_PREV = "pos_prev";
     private RecipeInfo mItem;
-    private View rootView;
+    public View rootView;
     private int currStep = 0;
     private Button nextButton;
     private Button backButton;
     private SimpleExoPlayer player;
     private PlayerView playerView;
-    boolean playWhenReady = false;
+    private RecyclerView mRecyclerView;
+    boolean playWhenReady = true;
     int currentWindow = 0;
     long playbackPosition = 0;
+    private List<StepInfo> mSteps;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -68,9 +73,9 @@ public class RecipeDetailFragment extends Fragment {
         if (getArguments().containsKey(ARG_ITEM_ID)) {
             int position = Integer.parseInt(getArguments().getString(ARG_ITEM_ID));
             mItem = RecipeListActivity.Recipes.get(position-1);
-
+            mSteps = mItem.recipeSteps;
             Activity activity = this.getActivity();
-            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            CollapsingToolbarLayout appBarLayout = activity.findViewById(R.id.toolbar_layout);
             if (appBarLayout != null) {
                 appBarLayout.setTitle(mItem.recipeName);
             }
@@ -85,12 +90,19 @@ public class RecipeDetailFragment extends Fragment {
         backButton = rootView.findViewById(R.id.back_step);
         playerView = rootView.findViewById(R.id.exo_video);
 
+        if (rootView.findViewById(R.id.detail_step_container) != null) {
+            mRecyclerView = rootView.findViewById(R.id.step_list);
+            setupRecyclerView(mRecyclerView);
+        }
         // Button Listeners derived from Slack conversation with Tane Tachyon
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 backButton.setVisibility(View.VISIBLE);
                 if(currStep + 1 <= mItem.recipeSteps.size() - 1) {
+                    currentWindow = 0;
+                    playbackPosition = 0;
+                    releasePlayer();
                     onClickNextStep(v);
                 }
 
@@ -104,6 +116,9 @@ public class RecipeDetailFragment extends Fragment {
             public void onClick(View v) {
                 nextButton.setVisibility(View.VISIBLE);
                 if(currStep >= 1) {
+                    currentWindow = 0;
+                    playbackPosition = 0;
+                    releasePlayer();
                     onClickBackStep(v);
                 } else {
                     backButton.setVisibility(View.INVISIBLE);
@@ -131,8 +146,17 @@ public class RecipeDetailFragment extends Fragment {
             RecipeWidget.setRecipeWidgetText(getContext(), ingredients);
 
             changeStep(currStep);
+
         }
+
         return rootView;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(POS_PREV, playbackPosition);
+        
     }
 
 
@@ -141,36 +165,45 @@ public class RecipeDetailFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        if (Util.SDK_INT > 23) {
-            initializePlayer();
-        }
+
+        initializePlayer();
     }
 
     @Override
     public void onResume() {
         super.onResume();
 
-        if ((Util.SDK_INT <= 23 || player == null)) {
-            initializePlayer();
-        }
+
+
+        changeStep(currStep);
+        initializePlayer();
     }
 
     @Override
     public void onPause() {
         super.onPause();
 
-        if (Util.SDK_INT <= 23) {
-            releasePlayer();
-        }
+        currentWindow = player.getCurrentWindowIndex();
+        playbackPosition = player.getCurrentPosition();
+        playWhenReady = player.getPlayWhenReady();
+
+        releasePlayer();
     }
 
     @Override
     public void onStop() {
         super.onStop();
 
-        if (Util.SDK_INT > 23) {
-            releasePlayer();
-        }
+        currentWindow = player.getCurrentWindowIndex();
+        playbackPosition = player.getCurrentPosition();
+        releasePlayer();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        releasePlayer();
     }
 
     private void releasePlayer() {
@@ -178,22 +211,27 @@ public class RecipeDetailFragment extends Fragment {
             playbackPosition = player.getCurrentPosition();
             currentWindow = player.getCurrentWindowIndex();
             playWhenReady = player.getPlayWhenReady();
+            player.stop();
             player.release();
             player = null;
         }
     }
 
-    private void initializePlayer() {
-        player = ExoPlayerFactory.newSimpleInstance(
-                new DefaultRenderersFactory(getContext()),
-                new DefaultTrackSelector(), new DefaultLoadControl());
-        playerView.setPlayer(player);
-        player.setPlayWhenReady(playWhenReady);
-        player.seekTo(currentWindow, playbackPosition);
+    synchronized private void initializePlayer() {
+        if (player == null) {
+            player = ExoPlayerFactory.newSimpleInstance(
+                    new DefaultRenderersFactory(getContext()),
+                    new DefaultTrackSelector(), new DefaultLoadControl());
+            playerView.setPlayer(player);
+            player.setPlayWhenReady(playWhenReady);
+            player.seekTo(currentWindow, playbackPosition);
+        } else {
+            player.seekTo(currentWindow, playbackPosition);
+        }
         StepInfo tempStep = mItem.recipeSteps.get(currStep);
         Uri uri = Uri.parse(tempStep.stepVideo);
         MediaSource video = buildMediaSource(uri);
-        player.prepare(video, true, false);
+        player.prepare(video, true, true);
     }
 
     private MediaSource buildMediaSource(Uri uri) {
@@ -211,7 +249,7 @@ public class RecipeDetailFragment extends Fragment {
         changeStep(currStep);
     }
 
-    private View changeStep(int id) {
+    public void changeStep(int id) {
         if (id <= mItem.recipeSteps.size()) {
             ImageView ivStepImage = rootView.findViewById(R.id.iv_step_image);
             StepInfo tempStep = mItem.recipeSteps.get(id);
@@ -240,12 +278,75 @@ public class RecipeDetailFragment extends Fragment {
 
             if (!tempStep.stepVideo.equals("")) {
                 playerView.setVisibility(View.VISIBLE);
-                initializePlayer();
             } else {
                 playerView.setVisibility(View.INVISIBLE);
                 releasePlayer();
             }
         }
-        return rootView;
+        initializePlayer();
+    }
+
+    public static class StepAdapter extends RecyclerView.Adapter<StepAdapter.ViewHolder> {
+        private List<StepInfo> mSteps = new ArrayList<>();
+        private RecipeDetailFragment mParentActivity;
+
+        StepAdapter(RecipeDetailFragment parent, List<StepInfo> items) {
+            this.mParentActivity = parent;
+            this.mSteps = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.recipe_detail, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mStepTitleView.setText(mSteps.get(position).stepTitle);
+            holder.mStepDescriptionView.setText(mSteps.get(position).stepDescription);
+
+            holder.itemView.setTag(mSteps.get(position));
+            holder.itemView.setOnClickListener(mOnClickListener);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mSteps.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            final TextView mStepTitleView;
+            final TextView mStepDescriptionView;
+            final TextView mStepIdView;
+
+            ViewHolder(View view) {
+                super(view);
+                mStepTitleView = view.findViewById(R.id.tv_step_title);
+                mStepDescriptionView = view.findViewById(R.id.tv_step_description);
+                mStepIdView = view.findViewById(R.id.tv_step_id);
+            }
+        }
+
+        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StepInfo item = (StepInfo) view.getTag();
+
+
+            }
+        };
+
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new StepAdapter(this, mSteps));
+    }
+
+    private void reloadAdapter() {
+        mRecyclerView.findViewById(R.id.step_list);
+        assert mRecyclerView != null;
+        setupRecyclerView((RecyclerView) mRecyclerView);
     }
 }
